a2cc50ef38238f602f24889c14d32505488ada66
AJIN R B <63920405+AJIN-98@users.noreply.github.com>
2024-01-02 01:43:24 +0530

    initial commit
    created springboot application for backend services.

---
+++ src/main/java/com/air/AirQualityBackend/AirQualityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.air.AirQualityBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityBackendApplication.class, args);
+	}
+
+}
